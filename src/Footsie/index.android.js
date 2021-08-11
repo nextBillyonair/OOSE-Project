065d2d3c8631067aa5f3ad29d64712f61a6735bd
Billy Watson <nextbillyonair@gmail.com>
2021-08-11 15:43:13 -0400

    Add files via upload

---
+++ src/Footsie/index.android.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {
+  AppRegistry,
+} from 'react-native';
+
+import Footsie from './src/index'
+
+AppRegistry.registerComponent('Footsie', () => Footsie);
